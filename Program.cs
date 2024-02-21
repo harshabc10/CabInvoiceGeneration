05245cc2a18adb8297115eec3cf889aa332abb20
harshabc10 <harshabc10@gmail.com>
2024-02-21 11:58:00 +0530

    pushed complete project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public  class InvoiceGenerator
+{
+    public const double CostPerKilometer = 10.0;
+    public const double CostPerMinute = 1.0;
+    public const double MinimumFare = 5.0;
+
+    public static double TotalFare(double distance, int timeInMinutes)
+    {
+        double fare = distance * CostPerKilometer + timeInMinutes * CostPerMinute;//d*km+t*
+
+        return Math.Max(fare, MinimumFare);
+    }
+}
